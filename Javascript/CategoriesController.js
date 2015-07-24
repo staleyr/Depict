f5c0a50b50d4f08b9aae72d5e57b9dcae683c17a
staleyr <staleyr@mail.gvsu.eduu>
2015-07-24 13:43:40 -0400

    Add new Depict page, and add footer template.

--- Javascript/CategoriesController.js
+++ Javascript/CategoriesController.js
@@ -3,6 +3,6 @@ var myApp = angular.module('depict',[]);
 myApp.controller('CategoriesController', ['$scope', function($scope) {
 
   // $scope.double = function(value) { return value * 2; };
-  $scope.categories = ["Sports", "Nature", "Abstract", "Black & White"];
+  $scope.categories = ["Sports", "Nature", "Abstract", "\Black And White"];
 
 }]);
---
+++ Javascript/Header-Footer.js
@@ -0,0 +1,23 @@
+
+  $(function(){
+
+    $("#header").append('\
+      \
+      <ul>\
+        <li><a href="Index.html">Home</a></li>\
+  			<li><a href="Categories.html">Categories</a></li>\
+  			<li><a href="#">Depicts</a></li>\
+      </ul>\
+      \
+      ');
+      
+      $("#footer").append('\
+      \
+      <ul>\
+        <li><a href="Index.html">About Us</a></li>\
+  			<li><a href="Categories.html">Contact Us</a></li>\
+      </ul>\
+      \
+      ');
+      
+  });
