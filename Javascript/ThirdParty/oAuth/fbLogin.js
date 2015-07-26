2f405b0408f2b204c08411dce6c2710c935cf7e5
Robert Staley <staleyr@mail.gvsu.edu>
2015-07-26 10:41:11 -0400

    Attempt to display logged in users name.

--- Javascript/ThirdParty/oAuth/fbLogin.js
+++ Javascript/ThirdParty/oAuth/fbLogin.js
@@ -34,6 +34,7 @@ function checkFbHashLogin() {
 function displayUser(user) {
     setTimeout(function () { }, 1000);
     if (user.id != null && user.id != "undefined") {
+      $("#userinfo").append("Hello " + user.id);
         //Do Stuff
         //You have access to user id, name, username, gender etc.
         //For more info visit https://developers.facebook.com/docs/reference/login/public-profile-and-friend-list/
