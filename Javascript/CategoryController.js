9d6a39dcec1afd655ec7e7fe86b0ec3019f217c8
staleyr <staleyr@mail.gvsu.eduu>
2015-07-27 13:30:45 -0400

    Fix array injection.

--- Javascript/CategoryController.js
+++ Javascript/CategoryController.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('depict');
 
-myApp.controller('CategoryController', ['$scope', function($scope, $routeParams) {
+myApp.controller('CategoryController', ['$scope', '$routeParams', function($scope, $routeParams) {
 
   $scope.category = $routeParams.category;
 
