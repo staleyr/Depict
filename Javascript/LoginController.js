b708a82c29e78adfd3afa0c6d3aad34920a650d0
staleyr <staleyr@mail.gvsu.eduu>
2015-07-28 12:58:42 -0400

    remove thens

--- Javascript/LoginController.js
+++ Javascript/LoginController.js
@@ -2,23 +2,7 @@ angular.module('depict')
   .controller('LoginController', function($scope, $auth) {
 
     $scope.authenticate = function(provider) {
-      $auth.authenticate(provider)
-        .then(function() {
-          $alert({
-            content: 'You have successfully logged in',
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        })
-        .catch(function(response) {
-          $alert({
-            content: response.data ? response.data.message : response,
-            animation: 'fadeZoomFadeDown',
-            type: 'material',
-            duration: 3
-          });
-        });
+      $auth.authenticate(provider);
     };
 
   });
